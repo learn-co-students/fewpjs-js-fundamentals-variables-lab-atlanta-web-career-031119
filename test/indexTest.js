243a5424a34e78dd9af607becf1e03a4ba805ba7
Shivang Dave <shivang.dave@flatironschool.com>
2019-04-22 18:29:40 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,9 @@
 const sinon = require( 'sinon' )
 
 describe('index.js', function () {
+  
   describe('companyName', function () {
+  
     it('is set as Scuber', function () {
       expect(companyName).to.equal('Scuber');
     });
